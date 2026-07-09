42f60dc8a221dec8e30340710d4a139dd4b901bf
Dharanivel P <dharanivelpvel@gmail.com>
2026-07-09 19:20:00 +0530

    Time: 0 ms (100%), Space: 19.8 MB (53.62%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -3,13 +3,10 @@ class Solution:
         pos=0
         even=len(nums)-1
         while pos<=even:
-            if nums[pos]%2!=0:
-                if nums[even]%2==0:
-                    nums[pos],nums[even]=nums[even],nums[pos]
-                    pos+=1
-                    even-=1
-                else:
-                    even-=1
-            else:
+            if (nums[pos]%2)>(nums[even]%2):
+                nums[pos],nums[even]=nums[even],nums[pos]
+            if (nums[pos]%2==0):
                 pos+=1
+            if(nums[even]%2==1):
+                even-=1
         return nums
