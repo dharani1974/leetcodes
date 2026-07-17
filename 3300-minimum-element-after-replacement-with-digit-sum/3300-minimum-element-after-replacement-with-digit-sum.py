5d3ad9ee0c3c227e9993c9e406c93b69f8a47ba1
Dharanivel P <dharanivelpvel@gmail.com>
2026-07-17 23:44:08 +0530

    Time: 2 ms (81.24%), Space: 19.3 MB (27.65%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
         for i in range(0,len(nums)):
-            if nums[i]>9:
-                st=str(nums[i])
-                temp=0
-                for j in range (0,len(st)):
-                    temp+=int(st[j])
-                nums[i]=temp
+            st=str(nums[i])
+            temp=0
+            for j in range (0,len(st)):
+                temp+=int(st[j])
+            nums[i]=temp
         return(min(nums))
