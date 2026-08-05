5346546785e71dce67553d586fe28fb743c62233
Dharanivel P <dharanivelpvel@gmail.com>
2026-08-05 21:20:35 +0530

    Time: 4 ms (28.16%), Space: 19.5 MB (33.15%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
@@ -1,23 +1,14 @@
 class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
-        if len(nums)==1:
-            return sorted(nums[0])
-        res=set()
-        set1=set(nums[1])
-        for i in range (len(nums[0])):
-            if nums[0][i] in set1:
-                res.add(nums[0][i]) 
-        if len(nums)>2:
-            i=2
-            while i<len(nums):
-                temp=set(nums[i])
-                rescopy=res.copy()
-                for x in res:
-                    if x not in temp:
-                        rescopy.remove(x)
-                res=rescopy
-                i+=1
-
-                
-        return sorted(list(res))
-        
+        seen={}
+        res=[]
+        for i in nums:
+            for j in i:
+                if j in seen:
+                    seen[j]+=1
+                else:
+                    seen[j]=1
+        for key,values in seen.items():
+            if values==len(nums):
+                res.append(key)
+        return sorted(res)
