bc3d0954e500d04027bbf0a805501abe5606be3f
Dharanivel P <dharanivelpvel@gmail.com>
2026-08-08 22:44:24 +0530

    Time: 17 ms (30.38%), Space: 19.3 MB (21.77%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,14 +1,15 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        a=0
-        v=0
-        for char in moves:
-            if char=="U":
-                v+=1
-            elif char=="D":
-                v-=1
-            elif char=="R":
-                a+=1
+        start=[0,0]
+        for ch in moves:
+            if ch=='U':
+                start[1]+=1
+            elif ch=='D':
+                start[1]-=1
+            elif ch=='L':
+                start[0]-=1
             else:
-                a-=1
-        return a==0 and v==0
+                start[0]+=1
+        if start==[0,0]:
+            return True
+        return False
